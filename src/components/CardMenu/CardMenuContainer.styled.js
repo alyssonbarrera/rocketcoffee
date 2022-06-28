afab8f3999ab8d7a2f32b5bfb841d1fe8ea448cc
Alysson Barrera <alyssonbarrera@outlook.com>
2022-06-27 22:29:48 -0300

    :lipstick: style update

--- src/components/CardMenu/CardMenuContainer.styled.js
+++ src/components/CardMenu/CardMenuContainer.styled.js
@@ -8,4 +8,20 @@ export const CardMenuContainerSection = styled.section`
 
     padding-bottom: 5rem;
     padding-inline: 2rem;
+
+    &:before {
+        content: '';
+        
+        position: fixed;
+        left: -29px;
+        bottom: 0;
+
+        width: 321px;
+        height: 321px;
+
+        background: #996DFF;
+        filter: blur(400px);
+
+        z-index: -1;
+    }
 `
